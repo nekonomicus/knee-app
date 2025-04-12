b1975c96c5ddaa2fea92b459140ac63532f0ca1e
nekonomicus <87386132+nekonomicus@users.noreply.github.com>
2025-04-12 21:03:50 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import KneeMeasurementInterface from './components/KneeMeasurementInterface';
+
+function App() {
+  return (
+    <div className="w-full">
+      <KneeMeasurementInterface />
+    </div>
+  );
+}
+
+export default App;
